975d02be9ad1e22c79e4bb757ac56b8c510c96c7
Gopal_Nayak <gopal32452@gmail.com>
2026-05-26 09:24:37 +0530

    Fix port variable

--- server.py
+++ server.py
@@ -849,4 +849,4 @@ if __name__ == "__main__":
     print(f"  Server     : http://localhost:5000")
     print("═"*55 + "\n")
     port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
